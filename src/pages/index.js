7311e36b3be0c3f1bb69bbd1de5063748aa75de4
Anand Ezhumalai <Anande@emids.com>
2019-03-17 00:16:06 +0530

    clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,10 @@
 import React from 'react'
 import Layout from '../components/layout'
-import Title from '../components/title'
-import ArticleList from '../components/article-list'
 import Carousel from '../components/carousel'
 
 export default () => (
   <Layout>
-    <Carousel />
-    
+    <Carousel />    
   </Layout>
 )
 
